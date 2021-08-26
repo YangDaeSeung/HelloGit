c013ca44c844ec436fa802a0ee5d3407ad2a9576
yds04 <yds04@192.168.219.101>
2021-08-26 10:06:09 +0900

    test() 모듈 완료

--- src/HelloGit/HelloWorld.java
+++ src/HelloGit/HelloWorld.java
@@ -7,6 +7,12 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("지금 git 수업 중");
 		System.out.println("집중하세요~~~ 단 한번만 수업합니다.");
+		test();
+	}
+
+	private static void test() {
+		// TODO Auto-generated method stub
+		System.out.println("새모듈작업");
 	}	
 
 }
