a6c8be899b4e5d903c2f6f9bfb1312d324a2389c
yds04 <yds04@192.168.219.101>
2021-08-26 09:48:53 +0900

    다시 한번 로컬 레파지토리에  커밋

--- src/HelloGit/HelloWorld.java
+++ src/HelloGit/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("지금 git 수업 중");
+		System.out.println("집중하세요~~~ 단 한번만 수업합니다.");
 	}	
 
 }
